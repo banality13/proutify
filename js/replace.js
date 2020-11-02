17f7e4e3a4517435dfe365aea351795c9198e118
theo1 <theo@framasoft.org>
2020-11-02 17:49:49 +0100

    Working (but dirty) example declaring a walker every look of array

--- js/replace.js
+++ js/replace.js
@@ -1,9 +1,26 @@
-var textNode;
-const walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-const rExp = new RegExp('Darmanin', 'gi');
+const replace = [
+  {
+    "name": "Darmanin",
+    "replaceWith": "Darmaprout"
+  },
+  {
+    "name": "Bruno Le Maire",
+    "replaceWith": "Bruno Le Prout"
+  },
+  {
+    "name": "Jean Castex",
+    "replaceWith": "Jean Proutex"
+  }
+];
 
-while (textNode = walk.nextNode()) {
-  textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Darmaprout');
-}
+replace.forEach((element) => {
+  var textNode;
+  let walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
 
-document.title = document.title.replace(rExp, 'Darmaprout');
+  const rExp = new RegExp(element['name'], 'gi');
+  document.title = document.title.replace(rExp, element['replaceWith']);
+
+  while (textNode = walk.nextNode()) {
+    textNode.nodeValue = textNode.nodeValue.replace(rExp, element['replaceWith']);
+  }
+})
